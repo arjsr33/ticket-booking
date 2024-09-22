3153e053d0d51fc162e67f45421e158a7fa68f63
arjsr33 <arjunsreedharan32@gmail.com>
2024-09-22 11:41:56 +0530

    Fully updated changes

--- Backend/server.js
+++ Backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(cors({
-  origin: 'https://chadnibooking.vercel.app/'
+  origin: 'https://chadnibooking.vercel.app'
 }));
 app.use(express.json());
 
