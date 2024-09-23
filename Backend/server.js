46d48b7ba7c6b0ddaa218786dfc3598a35253716
arjsr33 <arjunsreedharan32@gmail.com>
2024-09-23 22:51:54 +0530

    Fully updated changes

--- Backend/server.js
+++ Backend/server.js
@@ -6,7 +6,9 @@ require('dotenv').config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://chandnibookings.vercel.app/'
+}));
 app.use(express.json());
 
 // Connect to MongoDB
