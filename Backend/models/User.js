985bd15cdf9c774ec19afb18ac7d800bc903857f
arjsr33 <arjunsreedharan32@gmail.com>
2024-09-23 23:16:39 +0530

    Fully updated changes

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -4,9 +4,7 @@ const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, enum: ['user', 'admin'], default: 'user' },
-  isActive: { type: Boolean, default: true }
-
+  role: { type: String, enum: ['user', 'admin'], default: 'user' }
 });
 
 module.exports = mongoose.model('User', UserSchema);
